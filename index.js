ca1a2fceb6515db33f9e652045a466443c63f1eb
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-03-29 05:39:50 +0300

    Reduce method complete

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Code your solution here //
+
+const totalBatteries= batteryBatches.reduce(function(accumulator,currentValue){
+    return accumulator+currentValue
+},0)
+console.log(totalBatteries)
+const words = ["apple", "banana", "apple", "orange", "banana", "apple"];
+
